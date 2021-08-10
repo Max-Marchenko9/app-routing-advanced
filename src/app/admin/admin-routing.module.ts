84d59f1c0aff99b6f1178ada6d9091e2c5bc388d
Max-Marchenko9 <83778958+Max-Marchenko9@users.noreply.github.com>
2021-08-10 12:03:59 +0300

    add admin module + canActivate guard service

---
+++ src/app/admin/admin-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../Shared/auth.guard';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { ManagePhrasesComponent } from './manage-phrases/manage-phrases.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminHomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'phrases', component: ManagePhrasesComponent},
+      {path: 'users', component: ManageUsersComponent},
+      {path: '', redirectTo: 'phrases', pathMatch: 'full'}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
